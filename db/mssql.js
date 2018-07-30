312935132b92f7e36eaedce03b777da65023147a
Jackson LDH <ldh_mbpr2015@Jackson-no15inchi-makkubukku-puro.local>
2018-07-30 13:51:57 +0800

    mssql connected

--- db/mssql.js
+++ db/mssql.js
@@ -1,41 +1,94 @@
-var Connection = require("tedious").Connection;
-var Request = require("tedious").Request;
+// var Connection = require("tedious").Connection;
+// var Request = require("tedious").Request;
 
-var config = {
-  userName: "sa", // update me
-  password: "q1w2e3r4,k.l", // update me
-  server: "ldh7edd"
+// var config = {
+//   userName: "sa", // update me
+//   password: "q1w2e3r4,k.l", // update me
+//   server: "ldh7edd"
+// };
+
+// var connection = new Connection(config);
+
+// connection.on("connect", function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     executeStatement();
+//   }
+// });
+
+// function executeStatement() {
+//   request = new Request("select 123, 'hello world'", function(err, rowCount) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(rowCount + " rows");
+//     }
+//     connection.close();
+//   });
+
+//   request.on("row", function(columns) {
+//     columns.forEach(function(column) {
+//       if (column.value === null) {
+//         console.log("NULL");
+//       } else {
+//         console.log(column.value);
+//       }
+//     });
+//   });
+
+//   connection.execSql(request);
+// }
+
+const mssql = require("mssql");
+
+var dbConfig = {
+  server: "LDH7EDD\\SQLEXPRESS",
+  database: "MOPR",
+  user: "sa",
+  password: "q1w2e3r4,k.l",
+  port: 5555
 };
 
-var connection = new Connection(config);
+function getEmp() {
+  var conn = new mssql.ConnectionPool(dbConfig);
 
-connection.on("connect", function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    executeStatement();
-  }
-});
-
-function executeStatement() {
-  request = new Request("select 123, 'hello world'", function(err, rowCount) {
-    if (err) {
+  conn
+    .connect()
+    .then(function() {
+      var req = new mssql.Request(conn);
+      req
+        .query("SELECT * FROM 公會")
+        .then(function(recordset) {
+          console.log(recordset);
+          conn.close();
+        })
+        .catch(function(err) {
+          console.log(err);
+          conn.close();
+        });
+    })
+    .catch(function(err) {
       console.log(err);
-    } else {
-      console.log(rowCount + " rows");
-    }
-    connection.close();
-  });
-
-  request.on("row", function(columns) {
-    columns.forEach(function(column) {
-      if (column.value === null) {
-        console.log("NULL");
-      } else {
-        console.log(column.value);
-      }
     });
-  });
 
-  connection.execSql(request);
+  //--> another way
+  //var req = new sql.Request(conn);
+  //conn.connect(function (err) {
+  //    if (err) {
+  //        console.log(err);
+  //        return;
+  //    }
+  //    req.query("SELECT * FROM emp", function (err, recordset) {
+  //        if (err) {
+  //            console.log(err);
+  //        }
+  //        else {
+  //            console.log(recordset);
+  //        }
+  //        conn.close();
+  //    });
+  //});
 }
+
+getEmp();
